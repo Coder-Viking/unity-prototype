c87888a9028f34e0642bc73aa8a0fc1102deea48
Coder-Viking <120248615+Coder-Viking@users.noreply.github.com>
2025-08-07 01:46:39 -0700

    Add Item scriptable object definition (#13)

---
+++ Scripts/Item.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite icon;
+    public string description;
+}
